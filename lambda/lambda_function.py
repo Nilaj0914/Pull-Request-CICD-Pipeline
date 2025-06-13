8d47ca38977ef39e8580a40efef3dee17c794e50
Nilaj0914 <nilaj.majumder0814@gmail.com>
2025-06-13 11:05:16 +0530

    configured lambda file

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return{
         'statusCode': 200,
-        'body': json.dumps('Hello from new lambda')
+        'body': json.dumps('Hello lambda, this is a new workflow file')
     }
